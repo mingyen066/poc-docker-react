c61a8398d7a54c1be0c63a680d5dd0b623f9cfcb
Ming-Yen Chung <mingyen066@gmail.com>
2022-07-19 22:43:49 +0800

    new feature

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Fun!
+          I'm new Feature!
         </p>
         <a
           className="App-link"
